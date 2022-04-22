630ab7a6cb89fcb0022e571fc0cd20551d849df5
Axel Moncada <51040327+AxelMoncada@users.noreply.github.com>
2022-04-22 01:57:45 -0500

    Menu parte 1

---
+++ assets/function.js
@@ -0,0 +1,90 @@
+$(function(){
+
+  
+
+    $("#item2").mouseenter(function(){
+        $("#caption2").fadeIn();
+    });
+
+    $("#item2").mouseleave(function(){
+        $("#caption2").fadeOut();
+    });
+
+
+
+    $("#item1").mouseenter(function(){
+       $("#caption1").fadeIn();
+    });
+
+    $("#item1").mouseleave(function(){
+        $("#caption1").fadeOut();
+    });
+
+    
+
+    $("#item3").mouseenter(function(){
+        $("#caption3").fadeIn();
+     });
+ 
+     $("#item3").mouseleave(function(){
+         $("#caption3").fadeOut();
+     });
+
+     
+    $("#item4").mouseenter(function(){
+        $("#caption4").fadeIn();
+     });
+ 
+     $("#item4").mouseleave(function(){
+         $("#caption4").fadeOut();
+     });
+
+     
+    $("#item5").mouseenter(function(){
+        $("#caption5").fadeIn();
+     });
+ 
+     $("#item5").mouseleave(function(){
+         $("#caption5").fadeOut();
+     });
+
+     
+    $("#item6").mouseenter(function(){
+        $("#caption6").fadeIn();
+     });
+ 
+     $("#item6").mouseleave(function(){
+         $("#caption6").fadeOut();
+     });
+
+     
+    $("#item7").mouseenter(function(){
+        $("#caption7").fadeIn();
+     });
+ 
+     $("#item7").mouseleave(function(){
+         $("#caption7").fadeOut();
+     });
+
+     
+    $("#item8").mouseenter(function(){
+        $("#caption8").fadeIn();
+     });
+ 
+     $("#item8").mouseleave(function(){
+         $("#caption8").fadeOut();
+     });
+
+     
+    $("#item9").mouseenter(function(){
+        $("#caption9").fadeIn();
+     });
+ 
+     $("#item9").mouseleave(function(){
+         $("#caption9").fadeOut();
+     });
+
+
+
+
+});
